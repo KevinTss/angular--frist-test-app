f6db551eae1cba1e6faa2d10aa04964097e528db
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 15:41:25 +0200

    Bind data to template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses title</h2>'
+  template: '<h2>{{ title }}</h2>'
 })
 export class CoursesComponent {
+  title = "List of courses";
 }
