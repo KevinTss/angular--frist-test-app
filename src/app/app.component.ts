d80541f4b3a6438a38b51dffa59b75fb5aebea15
Kevin Tassi <kevin.tassi@gmail.com>
2019-07-22 22:14:14 +0200

    Bind & event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  isAuth = false;
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
